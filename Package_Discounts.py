77555080090a52cacc3e96ea2d8c16d1afc1a819
Kumori <tauen8@gmail.com>
2025-03-27 21:50:23 -0500

    added the package discount calculator with input validation

--- Package_Discounts.py
+++ Package_Discounts.py
@@ -1,23 +1,23 @@
 #Taylor Golden
 #Status: Complete
-
 UNIT_PRICE = 149.0
 
 num_packages = float(input("Enter the number of packages purchased"))
-
-if num_packages > 0:
-    subtotal = num_packages * UNIT_PRICE
-    if num_packages >= 150:
+while num_packages <=0:
+        print('Error: Incorrect number of packages')
+        num_packages = float(input("Enter the number of packages purchased"))
+subtotal = num_packages * UNIT_PRICE 
+if num_packages >= 150:
         discount = subtotal * 0.4
-    elif num_packages >=100:
+elif num_packages >=100:
         discount = subtotal * 0.3
-    elif num_packages >=50:
+elif num_packages >=50:
         discount = subtotal * 0.2
-    elif num_packages >=10:
+elif num_packages >=10:
         discount = subtotal *0.1
-    else:
-        discount = 0
-    
+else:
+    discount = 0
+              
 total = subtotal - discount
 
 print(f'Discount amount: {discount}\n Total after discount: {total}')
