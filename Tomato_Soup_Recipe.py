012a98cb04ce739f4e94f21c078624e128da3ce0
Kumori <tauen8@gmail.com>
2025-03-27 21:28:20 -0500

    added two decimal place f strings

--- Tomato_Soup_Recipe.py
+++ Tomato_Soup_Recipe.py
@@ -8,5 +8,5 @@ num_servings = float(input('Enter the number of servings you want to make '))
 
 serving_amt = [x * num_servings for x in ingredients]
 
-print(f"To make {num_servings} servings of {dish_name} you will need:\n {serving_amt[0]} cups of tomato sauce\n{serving_amt[1]} cups of tomato paste\n {serving_amt[2]} cloves of garlic and \n {serving_amt[3]} tablespoons of oregano ")
+print(f"To make {num_servings} servings of {dish_name} you will need:\n {serving_amt[0]:.2f} cups of tomato sauce\n{serving_amt[1]:.2f} cups of tomato paste\n {serving_amt[2]:.2f} cloves of garlic and \n {serving_amt[3]:.2f} tablespoons of oregano ")
 
